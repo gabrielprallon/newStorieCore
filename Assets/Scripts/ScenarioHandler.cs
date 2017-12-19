ead204f829b82a36ef7c5623e71bdb88d3de8cb7
gabrielprallon <gabrielprallon@gmail.com>
2017-12-19 07:55:40 -0200

    some new parameters on the articy:draft

---
+++ Assets/Scripts/ScenarioHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Articy;
+
+
+public class ScenarioHandler : MonoBehaviour {
+
+
+    [SerializeField]
+    private int m_lastScenario;
+    [SerializeField]
+    public List<GameObject> m_scenarioList;
+
+	// Use this for initialization
+	void Start () {
+		
+         // m_lastScenario = Articy.ProjectTheseus.GlobalVariables.ArticyGlobalVariables.Default.SVar.m_Scenario;
+            
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*if(m_lastScenario != Articy.ProjectTheseus.GlobalVariables.ArticyGlobalVariables.Default.SVar.m_Scenario)
+        {
+            m_scenarioList[m_lastScenario].SetActive(false);
+            m_scenarioList[Articy.ProjectTheseus.GlobalVariables.ArticyGlobalVariables.Default.SVar.m_Scenario].SetActive(true);
+            m_lastScenario = Articy.ProjectTheseus.GlobalVariables.ArticyGlobalVariables.Default.SVar.m_Scenario;
+        }
+        */
+	}
+}
